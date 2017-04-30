76a60b9a8b51c73428c63b8b7aafd66e0188ded2
Jason Manns <Jason.Manns@gmx.com>
2017-04-30 21:11:54 +0100

    Add basic functionality and styling

--- script.js
+++ script.js
@@ -1,43 +1,97 @@
 var total = ''
 var num = ''
-var operand = false;
+var operand = undefined
+var expression = ''
 
-(function () {
+;;(function () {
   const buttons = document.querySelectorAll('button')
-  const screen = document.querySelector('.screen')
+  const screen = document.querySelector('.screen.top')
+  const screenBottom = document.querySelector('.screen.bottom')
 
   buttons.forEach(button => {
     button.addEventListener('click', (e) => {
+      const input = e.target.dataset.val
       
-
-      
-      let input = e.target.dataset.val
-      if (isNumber(input) || input === '.') {
-        if (operand) {
-          num = num + input
-          screen.value = num
+      if (isClear(input)) {
+        reset()
+      }
+    
+      // handle operand
+      if (isOperand(input) && total) {
+        if (isOperand(expression.slice(-1))) {
+          expression = expression.slice(0, -1) + input
         } else {
-          total = total + input
-          screen.value = total
+          expression = expression + input
+        }
+        if (operand && num) {
+          total = eval(`${total} ${operand} ${num}`)
+          screenBottom.value = total
+          num = ''
+        }
+        operand = input
+      }
+      
+      // handle number
+      if (isNumber(input) || isDecimalPoint(input)) {
+        if (!operand) {
+          if (isDecimalPoint(input)) {
+            if (!hasDecimalPoint(total)) {
+              total += input
+              expression = expression + input
+            }
+          } else {
+            if (total === '0') {
+              total = input
+            } else {
+              total = total + input
+            }
+            expression = expression + input
+          }
+            
+        } else {
+          if (isDecimalPoint(input)) {
+            if (!hasDecimalPoint(num)) {
+              num += input
+              expression = expression + input
+            }
+          } else {
+            if (num === '0') {
+              num = input
+            } else {
+              num = num + input
+            }
+            expression = expression + input
+          }
+          screenBottom.value = eval(`${total} ${operand} ${num}`)
+        }
+      }
+      
+      // handleEquals
+      if (isEquals(input)) {
+        if (total && num) {
+          total = eval(`${total} ${operand} ${num}`)
+          num = ''
+          operand = undefined
+          expression = total + ''
+          screenBottom.value = ''
+        } else {
+          expression = total
         }
         
-      } else if (isOperand(input)) {
-        screen.value = input
-        if (operand) {
-          total = eval(`${total} ${operand} ${num}`)
-          operand = input
-        } else {
-          operand = input
-        }
-        num = ''
       }
       
       
+      screen.value=expression
       
-      
-      console.log(total)
+      console.log('TOTAL', total)
+      console.log('NUM', num)
+      console.log('OPERAND', operand)
     })
   })
+  
+  function isClear (str) {
+    return str === 'C'
+  }
 
 
   function isNumber (str) {
@@ -45,6 +99,28 @@ var operand = false;
   }
   
   function isOperand (str) {
-    return str === '+' || str === '-' || str === '*' || str === '%'
+    return str === '+' || str === '-' || str === '*' || str === '/' || str === '%'
+  }
+  
+  function isEquals (str) {
+    return str === '='
+  }
+  
+  function isDecimalPoint (str) {
+    return str === '.'
+  }
+  
+  function hasDecimalPoint (str) {
+    console.log(str.includes('.'))
+    return str.includes('.')
+  }
+  
+  function reset () {
+    total = ''
+    num = ''
+    operand = undefined
+    expression = ''
+    screen.value = ''
+    screenBottom.value = ''
   }
 })()
